826406d125407b660614cad48f360f311e58f895
Gabriel_31997 <gabrielwkun@gmail.com>
2026-09-14 09:16:01 +0000

    deployment

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -5,7 +5,7 @@ class Banner extends React.Component {
     return (
       <>
         <div
-          className="flex flex-wrap md:bg-fixed md:w-full object-cover object-center justify-center  h-100 md:h-110 bg-gray-400 mx-0 bg-no-repeat bg-cover bg-center"
+          className="flex flex-wrap bg-[url('/hero.gif')] md:bg-fixed md:w-full object-cover object-center justify-center h-100 md:h-110 bg-gray-400 mx-0 bg-no-repeat bg-cover bg-center"
           id="banner"
         >
           <div className="md:pt-32 pt-8 text-center mx-5">
